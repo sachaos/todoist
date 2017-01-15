535b6c608bc1d7fdec4f3e4c46957bfe6f27562f
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2017-01-15 23:57:15 +0900

    :recycle: DRY shell scripts

--- todoist_functions.sh
+++ todoist_functions.sh
@@ -1,17 +1,24 @@
 select_items_command="todoist list | peco | cut -d ' ' -f 1 | tr '\n' ' '"
 
-# todoist find item
-function peco-todoist-item () {
-    local SELECTED_ITEMS="$(eval ${select_items_command})"
-    if [ -n "$SELECTED_ITEMS" ]; then
+function insert-in-buffer () {
+    if [ -n "$1" ]; then
+        local new_left=""
         if [ -n "$LBUFFER" ]; then
-            local new_left="${LBUFFER%\ } $SELECTED_ITEM"
-        else
-            local new_left="$SELECTED_ITEMS"
+            new_left="${new_left}${LBUFFER} "
         fi
+        if [ -n "$2" ]; then
+            new_left="${new_left}${2} "
+        fi
+        new_left="${new_left}$1"
         BUFFER=${new_left}${RBUFFER}
         CURSOR=${#new_left}
     fi
+}
+
+# todoist find item
+function peco-todoist-item () {
+    local SELECTED_ITEMS="$(eval ${select_items_command})"
+    insert-in-buffer $SELECTED_ITEMS
 }
 zle -N peco-todoist-item
 bindkey "^xtt" peco-todoist-item
@@ -19,15 +26,7 @@ bindkey "^xtt" peco-todoist-item
 # todoist find project
 function peco-todoist-project () {
     local SELECTED_PROJECT="$(todoist projects | peco | head -n1 | cut -d ' ' -f 1)"
-    if [ -n "$SELECTED_PROJECT" ]; then
-        if [ -n "$LBUFFER" ]; then
-            local new_left="${LBUFFER%\ } -P $SELECTED_PROJECT"
-        else
-            local new_left="-P $SELECTED_PROJECT"
-        fi
-        BUFFER=${new_left}${RBUFFER}
-        CURSOR=${#new_left}
-    fi
+    insert-in-buffer "${SELECTED_PROJECT}" "-P"
 }
 zle -N peco-todoist-project
 bindkey "^xtp" peco-todoist-project
@@ -35,27 +34,23 @@ bindkey "^xtp" peco-todoist-project
 # todoist find labels
 function peco-todoist-labels () {
     local SELECTED_LABELS="$(todoist labels | peco | cut -d ' ' -f 1 | tr '\n' ',' | sed -e 's/,$//')"
-    if [ -n "$SELECTED_LABELS" ]; then
-        if [ -n "$LBUFFER" ]; then
-            local new_left="${LBUFFER%\ } -L $SELECTED_LABELS"
-        else
-            local new_left="-L $SELECTED_LABELS"
-        fi
-        BUFFER=${new_left}${RBUFFER}
-        CURSOR=${#new_left}
-    fi
+    insert-in-buffer "${SELECTED_LABELS}" "-L"
 }
 zle -N peco-todoist-labels
 bindkey "^xtl" peco-todoist-labels
 
+function todoist-exec-with-select-task () {
+    if [ -n "$2" ]; then
+        BUFFER="todoist $1 $(echo "$2" | tr '\n' ' ')"
+        CURSOR=$#BUFFER
+        zle accept-line
+    fi
+}
+
 # todoist close
 function peco-todoist-close() {
     local SELECTED_ITEMS="$(eval ${select_items_command})"
-    if [ -n "$SELECTED_ITEMS" ]; then
-        BUFFER="todoist close $(echo "$SELECTED_ITEMS" | tr '\n' ' ')"
-        CURSOR=$#BUFFER
-    fi
-    zle accept-line
+    todoist-exec-with-select-task close $SELECTED_ITEMS
 }
 zle -N peco-todoist-close
 bindkey "^xtc" peco-todoist-close
@@ -63,11 +58,7 @@ bindkey "^xtc" peco-todoist-close
 # todoist delete
 function peco-todoist-delete() {
     local SELECTED_ITEMS="$(eval ${select_items_command})"
-    if [ -n "$SELECTED_ITEMS" ]; then
-        BUFFER="todoist delete $(echo "$SELECTED_ITEMS" | tr '\n' ' ')"
-        CURSOR=$#BUFFER
-    fi
-    zle accept-line
+    todoist-exec-with-select-task delete $SELECTED_ITEMS
 }
 zle -N peco-todoist-delete
 bindkey "^xtk" peco-todoist-delete
