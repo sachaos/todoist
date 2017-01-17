4433488e7419706e281913e27c73ffdd509bc91d
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2017-01-17 12:12:28 +0900

    :+1: Output error on sync command

--- sync.go
+++ sync.go
@@ -10,11 +10,11 @@ func Sync(c *cli.Context) (todoist.Sync, error) {
 	var sync todoist.Sync
 	sync, err := todoist.SyncAll(viper.GetString("token"))
 	if err != nil {
-		return sync, CommandFailed
+		return sync, err
 	}
 	err = WriteCache(default_cache_path, sync)
 	if err != nil {
-		return sync, CommandFailed
+		return sync, err
 	}
 
 	return sync, nil
