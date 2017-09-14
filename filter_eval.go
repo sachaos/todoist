e1855e12bb44f9b153f697814141c36ad7f95e3b
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2017-09-14 23:23:55 +0900

    :+1: Implement BoolInfixOpExpr Evaluator

--- filter_eval.go
+++ filter_eval.go
@@ -13,6 +13,19 @@ var priorityRegex = regexp.MustCompile("^p([1-4])$")
 func Eval(e Expression, item todoist.Item) (result bool, err error) {
 	result = false
 	switch e.(type) {
+	case BoolInfixOpExpr:
+		e := e.(BoolInfixOpExpr)
+		lr, err := Eval(e.left, item)
+		rr, err := Eval(e.right, item)
+		if err != nil {
+			return false, nil
+		}
+		switch e.operator {
+		case '&':
+			return lr && rr, nil
+		case '|':
+			return lr || rr, nil
+		}
 	case StringExpr:
 		matched := priorityRegex.FindStringSubmatch(e.(StringExpr).literal)
 
