033727d57fb3214c362aca1a3a57eab1b9bcef39
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2017-09-16 21:33:21 +0900

    :green_heart: Add test of filter_parser

---
+++ filter_parser_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test ...
+func TestFilter(t *testing.T) {
+	assert.Equal(t, nil, Filter(""), "the should be equal")
+	assert.Equal(t, StringExpr{literal: "p1"}, Filter("p1"), "the should be equal")
+
+	assert.Equal(t,
+		BoolInfixOpExpr{
+			left:     StringExpr{literal: "p1"},
+			operator: '|',
+			right:    StringExpr{literal: "p2"},
+		},
+		Filter("p1 | p2"), "the should be equal")
+
+	assert.Equal(t,
+		BoolInfixOpExpr{
+			left:     StringExpr{literal: "p1"},
+			operator: '&',
+			right:    StringExpr{literal: "p2"},
+		},
+		Filter("p1 & p2"), "the should be equal")
+
+	assert.Equal(t,
+		BoolInfixOpExpr{
+			left:     StringExpr{literal: "p1"},
+			operator: '&',
+			right: BoolInfixOpExpr{
+				left:     StringExpr{literal: "p2"},
+				operator: '|',
+				right:    StringExpr{literal: "p3"},
+			},
+		},
+		Filter("p1 & (p2 | p3 )"), "the should be equal")
+}
