ada3e6691a201b80963f12fe7a314e327f40b25d
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2017-10-02 17:44:01 +0900

    :shirt: Fix typo

--- filter_parser_test.go
+++ filter_parser_test.go
@@ -10,11 +10,11 @@ import (
 
 // Test ...
 func TestFilter(t *testing.T) {
-	assert.Equal(t, nil, Filter(""), "the should be equal")
+	assert.Equal(t, nil, Filter(""), "they should be equal")
 }
 
 func TestPriorityFilter(t *testing.T) {
-	assert.Equal(t, StringExpr{literal: "p1"}, Filter("p1"), "the should be equal")
+	assert.Equal(t, StringExpr{literal: "p1"}, Filter("p1"), "they should be equal")
 }
 
 func TestBoolInfixFilter(t *testing.T) {
@@ -24,7 +24,7 @@ func TestBoolInfixFilter(t *testing.T) {
 			operator: '|',
 			right:    StringExpr{literal: "p2"},
 		},
-		Filter("p1 | p2"), "the should be equal")
+		Filter("p1 | p2"), "they should be equal")
 
 	assert.Equal(t,
 		BoolInfixOpExpr{
@@ -32,7 +32,7 @@ func TestBoolInfixFilter(t *testing.T) {
 			operator: '&',
 			right:    StringExpr{literal: "p2"},
 		},
-		Filter("p1 & p2"), "the should be equal")
+		Filter("p1 & p2"), "they should be equal")
 
 	assert.Equal(t,
 		BoolInfixOpExpr{
@@ -44,7 +44,7 @@ func TestBoolInfixFilter(t *testing.T) {
 				right:    StringExpr{literal: "p3"},
 			},
 		},
-		Filter("p1 & (p2 | p3 )"), "the should be equal")
+		Filter("p1 & (p2 | p3 )"), "they should be equal")
 }
 
 func setNow(t time.Time) {
@@ -57,41 +57,41 @@ func TestDateTimeFilter(t *testing.T) {
 
 	assert.Equal(t,
 		time.Date(2017, time.October, 5, 0, 0, 0, 0, time.Local),
-		Filter("10/5/2017"), "the should be equal")
+		Filter("10/5/2017"), "they should be equal")
 
 	assert.Equal(t,
 		time.Date(timeNow.Year(), time.January, 3, 0, 0, 0, 0, time.Local),
-		Filter("Jan 3"), "the should be equal")
+		Filter("Jan 3"), "they should be equal")
 
 	assert.Equal(t,
 		time.Date(timeNow.Year(), time.August, 8, 0, 0, 0, 0, time.Local),
-		Filter("8 August"), "the should be equal")
+		Filter("8 August"), "they should be equal")
 
 	assert.Equal(t,
 		time.Date(2020, time.February, 10, 0, 0, 0, 0, time.Local),
-		Filter("10 Feb 2020"), "the should be equal")
+		Filter("10 Feb 2020"), "they should be equal")
 
 	assert.Equal(t,
 		time.Date(timeNow.Year(), time.May, 16, 0, 0, 0, 0, time.Local),
-		Filter("16/05"), "the should be equal")
+		Filter("16/05"), "they should be equal")
 
 	assert.Equal(t,
 		time.Date(timeNow.Year(), timeNow.Month(), timeNow.Day(), 16, 0, 0, 0, time.Local),
-		Filter("16:00"), "the should be equal")
+		Filter("16:00"), "they should be equal")
 
 	assert.Equal(t,
 		time.Date(timeNow.Year(), timeNow.Month(), timeNow.Day(), 16, 10, 3, 0, time.Local),
-		Filter("16:10:03"), "the should be equal")
+		Filter("16:10:03"), "they should be equal")
 
 	assert.Equal(t,
 		time.Date(timeNow.Year(), timeNow.Month(), timeNow.Day(), 15, 0, 0, 0, time.Local),
-		Filter("3pm"), "the should be equal")
+		Filter("3pm"), "they should be equal")
 
 	assert.Equal(t,
 		time.Date(timeNow.Year(), timeNow.Month(), timeNow.Day(), 7, 0, 0, 0, time.Local),
-		Filter("7am"), "the should be equal")
+		Filter("7am"), "they should be equal")
 
 	assert.Equal(t,
 		time.Date(2020, time.February, 10, 15, 0, 0, 0, time.Local),
-		Filter("10 Feb 2020 3pm"), "the should be equal")
+		Filter("10 Feb 2020 3pm"), "they should be equal")
 }
