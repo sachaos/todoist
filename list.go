ec2bbb6a3f93c1f02180c9e7c221503d27bf22d3
Salahutdinov Dmitry <dsalahutdinov@gmail.com>
2017-11-13 16:53:53 +0500

    chore: minor optimization for long task lists

--- list.go
+++ list.go
@@ -9,15 +9,17 @@ func List(c *cli.Context) error {
 	client := GetClient(c)
 
 	colorList := ColorList()
-	var projectIds []int
-	for _, project := range client.Store.Projects {
-		projectIds = append(projectIds, project.GetID())
+	projectsCount := len(client.Store.Projects)
+	projectIds := make([]int, projectsCount, projectsCount)
+	for i, project := range client.Store.Projects {
+		projectIds[i] = project.GetID()
 	}
 	projectColorHash := GenerateColorHash(projectIds, colorList)
 	ex := Filter(c.String("filter"))
 
-	itemList := [][]string{}
-	for _, itemOrder := range client.Store.ItemOrders {
+  orderItemsCount := len(client.Store.ItemOrders)
+	itemList := make([][]string, orderItemsCount, orderItemsCount)
+	for i, itemOrder := range client.Store.ItemOrders {
 		item := itemOrder.Data.(todoist.Item)
 		r, err := Eval(ex, item)
 		if err != nil {
@@ -26,14 +28,14 @@ func List(c *cli.Context) error {
 		if !r || item.Checked == 1 {
 			continue
 		}
-		itemList = append(itemList, []string{
+		itemList[i] = []string{
 			IdFormat(item),
 			PriorityFormat(item.Priority),
 			DueDateFormat(item.DueDateTime(), item.AllDay),
 			ProjectFormat(item.ProjectID, client.Store.Projects, projectColorHash, c),
 			item.LabelsString(client.Store.Labels),
 			ContentPrefix(client.Store.Items, item, c) + ContentFormat(item),
-		})
+		}
 	}
 
 	defer writer.Flush()
