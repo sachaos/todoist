0ef582a1772ca0ca418751464a72f31984651ac6
hokorobi <hokorobi.hokorobi@gmail.com>
2019-01-19 19:16:15 +0900

    Fix multiple-value uuid.NewV4 Makefile error #61

--- lib/command.go
+++ lib/command.go
@@ -17,9 +17,11 @@ type Command struct {
 type Commands []Command
 
 func NewCommand(command_type string, command_args interface{}) Command {
+	id, _ := uuid.NewV4()
+	tempid, _ := uuid.NewV4()
 	return Command{
-		UUID:   uuid.NewV4().String(),
-		TempID: uuid.NewV4().String(),
+		UUID:   id.String(),
+		TempID: tempid.String(),
 		Type:   command_type,
 		Args:   command_args,
 	}
