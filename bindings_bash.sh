91316079af5ae2f2148cf1c8efe70fed904a7a2c
Semar Augusto <semar@most.com.br>
2019-08-05 14:14:39 -0300

    Add aliases and bash bindings.

---
+++ bindings_bash.sh
@@ -0,0 +1,5 @@
+bind '"\C-xtc": "todoist list | fzf | cut -d \" \" -f 1 | xargs -I {} todoist close "{}"\n" '
+bind '"\C-xtk": "todoist list | fzf | cut -d \" \" -f 1 | xargs -I {} todoist delete "{}"\n" '
+bind '"\C-xtm": "todoist list | fzf | cut -d \" \" -f 1 | xargs -I {} todoist modify \"{}\" -c " '
+bind '"\C-xtl": "todoist list | fzf | cut -d \" \" -f 1 | xargs -I {} todoist show "{}"\n"'
+bind '"\C-xtp": "todoist --project-namespace projects | fzf | cut -d \" \" -f 2 | xargs -I {} todoist list -f "{}"\n"'
