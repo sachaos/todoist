b325cacd2f03ba2a158c878a92c20cbfb5ff85de
Semar Augusto <semar@most.com.br>
2019-08-05 14:20:01 -0300

    remove problematic binding

--- bindings_bash.sh
+++ bindings_bash.sh
@@ -2,4 +2,3 @@ bind '"\C-xtc": "todoist list | fzf | cut -d \" \" -f 1 | xargs -I {} todoist cl
 bind '"\C-xtk": "todoist list | fzf | cut -d \" \" -f 1 | xargs -I {} todoist delete "{}"\n" '
 bind '"\C-xtm": "todoist list | fzf | cut -d \" \" -f 1 | xargs -I {} todoist modify \"{}\" -c " '
 bind '"\C-xtl": "todoist list | fzf | cut -d \" \" -f 1 | xargs -I {} todoist show "{}"\n"'
-bind '"\C-xtp": "todoist --project-namespace projects | fzf | cut -d \" \" -f 2 | xargs -I {} todoist list -f "{}"\n"'
