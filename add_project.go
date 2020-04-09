515776f1d7534d0c0660ddb66cc309eddeda61ad
Patryk Niedzwiedzinski <pniedzwiedzinski19@gmail.com>
2020-04-09 11:19:48 +0200

    Add project

---
+++ add_project.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+
+	"github.com/sachaos/todoist/lib"
+	"github.com/urfave/cli"
+)
+
+func AddProject(c *cli.Context) error {
+	client := GetClient(c)
+
+	project := todoist.Project{}
+	if !c.Args().Present() {
+		return CommandFailed
+	}
+
+	project.Name = c.Args().First()
+	project.Color = c.Int("color")
+	project.ItemOrder = c.Int("item-order")
+
+	if err := client.AddProject(context.Background(), project); err != nil {
+		return err
+	}
+
+	return Sync(c)
+}
